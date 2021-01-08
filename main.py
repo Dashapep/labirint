1d64bf474554073acb2c6325de14783deb5b3866
Dashapep <pds032005@gmail.com>
2021-01-09 00:07:49 +0300

    добавлена открытая рамка и рандомное выставление бонусов

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import pygame
 import os
 import sys
 import time
+import random
 
 # fail = input()
-fail = 'lab+gb.png'
+# fail = 'lab+gb.png'
+# зададим список доступных лабиринтов
+spisok = ['lab1+gb.png', 'lab2+gb.png', 'lab3+gb.png', 'lab4+gb.png', 'lab5+gb.png', 'lab6+gb.png']
+fail = 'lab1+gb.png'
 
 pygame.init()
 size = width, height = 85 * 16, 50 * 16
-screen = pygame.display.set_mode(size)
-# screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+# screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.display.set_caption('Program')
 FPS = 1
 
@@ -58,25 +62,31 @@ class Tile(pygame.sprite.Sprite):
         self.image = tile_images[tile_type]
         self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
 
+
 class Darknes(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(darknes_group, all_sprites)
         self.image = tile_images[tile_type]
         self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(player_group, all_sprites)
         self.image = player_image
         self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
-        self.lightrect = pygame.Rect((pos_x - 2 * tile_width, pos_y - 2 * tile_height, 5 * tile_width, 5 * tile_height))
+        self.lightrect = pygame.Rect((pos_x - 1 * tile_width, pos_y - 1 * tile_height, 5 * tile_width, 5 * tile_height))
+        self.x = pos_x
+        self.y = pos_y
 
     def update(self, x, y):
         global time
+        global vremya
+        dx = x // tile_width
+        dy = y // tile_height
         if 0 <= self.rect.y + y < height and 0 <= self.rect.x + x < width and \
                 level_map[(self.rect.y + y) // tile_height][(self.rect.x + x) // tile_width] in ('.', '@', ')', '('):
             self.rect = self.rect.move(x, y)
-            # self.lightrect = pygame.Rect((x - 2 * tile_width, y - 2 * tile_height, 5 * tile_width, 5 * tile_height))
             self.lightrect = self.lightrect.move(x, y)
             if level_map[(self.rect.y) // tile_height][(self.rect.x) // tile_width] == ')':
                 time += 15
@@ -88,7 +98,10 @@ class Player(pygame.sprite.Sprite):
                 level_map[(self.rect.y) // tile_height][(self.rect.x) // tile_width] = '.'
             else:
                 pass
+            self.x += dx
+            self.y += dy
             remove_dark()
+
 
 def remove_bonus():
     for bonus_elem in tiles_group:
@@ -98,11 +111,11 @@ def remove_bonus():
             bonus_elem.kill()
             Tile('empty', x, y)
 
+
 def remove_dark():
     for dark_elem in darknes_group:
         if player.lightrect.collidepoint(dark_elem.rect.center):
             dark_elem.kill()
-
 
 
 clock = pygame.time.Clock()
@@ -120,6 +133,8 @@ def load_level(filename):
     fullname = os.path.join('imgs', filename)
     a = Image.open(fullname)
     sh, d = a.size
+    count_good = 10
+    count_bad = 10
     karta = []
     for y in range(5, d, 14):
         stroka = []
@@ -140,6 +155,20 @@ def load_level(filename):
             else:
                 stroka += '.'
         karta.append(stroka)
+    shir_kart = len(karta[0])
+    dlin_kart = len(karta)
+    while count_bad > 0:
+        x = random.randint(1, shir_kart - 2)
+        y = random.randint(1, dlin_kart - 2)
+        if karta[y][x] == '.':
+            karta[y][x] = '('
+            count_bad -= 1
+    while count_good > 0:
+        x = random.randint(1, shir_kart - 2)
+        y = random.randint(1, dlin_kart - 2)
+        if karta[y][x] == '.':
+            karta[y][x] = ')'
+            count_good -= 1
 
     return karta
 
@@ -152,11 +181,17 @@ def load_level(filename):
 # return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 def Show_timer(time):
-    font = pygame.font.Font(None, 50)
+    font = pygame.font.Font(None, 30)
     text = font.render(str(time), True, (255, 100, 100))
+    tvremya = font.render(str(vremya), True, (100, 255, 100))
+    textvremya = font.render('Прошло: ', True, (100, 255, 100))
+    texttime = font.render('Осталось: ', True, (255, 100, 100))
     text_x = width - 80
-    text_y = 30
+    text_y = 20
     screen.blit(text, (text_x, text_y))
+    screen.blit(texttime, (text_x - 100, text_y))
+    screen.blit(tvremya, (text_x, text_y + 30))
+    screen.blit(textvremya, (text_x - 100, text_y + 30))
     # pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
     #                                        text_w + 20, text_h + 20), 1)
 
@@ -176,7 +211,8 @@ def generate_level(level):
                 Tile('good', x, y)
             elif level[y][x] == '(':
                 Tile('bad', x, y)
-            Darknes('dark', x, y)
+            if not (x <= 1 or y <= 1 or x >= len(level[y]) - 2 or y >= len(level) - 2):
+                Darknes('dark', x, y)
     return new_player, x, y
 
 
@@ -185,6 +221,7 @@ player, level_x, level_y = generate_level(level_map)
 run = True
 win = False
 time = 180
+vremya = 0
 Show_timer(time)
 pygame.time.set_timer(pygame.USEREVENT, 1000)
 remove_dark()
@@ -203,8 +240,18 @@ while run:
             player.update(tile_width, 0)
         if key[pygame.K_ESCAPE]:
             run = False
+        if key[pygame.K_F10]:
+            for dark_elem in darknes_group:
+                dark_elem.kill()
+
         if event.type == pygame.USEREVENT:
             time -= 1
+            if time <= 0:
+                run = False
+            vremya += 1
+        if player.y >= level_y or player.x >= level_x:
+            run = False
+            win = True
     clock.tick(50)
     screen.fill((0, 0, 0))
     tiles_group.draw(screen)
